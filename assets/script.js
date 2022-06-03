ef6431a352dbe9f93e28503bf0b1b6627646bea2
panecatl <ypanecatl24@gmail.com>
2022-06-03 15:12:19 -0500

    fixed display on html and js troubleshoot

--- assets/script.js
+++ assets/script.js
@@ -41,16 +41,16 @@ $(document).ready(function() {
         })
     }
     // getting items from local storage 
-    $("#time8 .description").val(localStorage.getItem("time8"));
-    $("#time9 .description").val(localStorage.getItem("time9"));
-    $("#time10 .description").val(localStorage.getItem("time10"));
-    $("#time11 .description").val(localStorage.getItem("time11"));
-    $("#time12 .description").val(localStorage.getItem("time12"));
-    $("#time1 .description").val(localStorage.getItem("time1"));
-    $("#time2 .description").val(localStorage.getItem("time2"));
-    $("#time3 .description").val(localStorage.getItem("time3"));
-    $("#time4 .description").val(localStorage.getItem("time4"));
-    $("#time5 .description").val(localStorage.getItem("time5"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     timeTracker();
 })
